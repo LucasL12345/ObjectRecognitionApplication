d9b4463c864671eee1eea3fc4d2149aade62b172
Lucas <lucaslemoine49@gmail.com>
2023-02-20 01:02:36 +0000

    changed app information text

--- ObjectRecognitionApplication/InformationViewController.swift
+++ ObjectRecognitionApplication/InformationViewController.swift
@@ -19,31 +19,31 @@ class InformationViewController: UIViewController {
         
         NSLayoutConstraint.activate([
             backButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
-            backButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 30),
+            backButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 20),
             backButton.widthAnchor.constraint(equalToConstant: 150),
             backButton.heightAnchor.constraint(equalToConstant: 80),
         ])
         
         let titleLabel = UILabel()
         titleLabel.text = "App Information"
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 32)
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 28)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(titleLabel)
         
         let paragraphLabel = UILabel()
-        paragraphLabel.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fringilla orci ac leo aliquet congue. Nulla non blandit justo. Nullam non mauris pellentesque, placerat nulla vel, semper libero. Nunc et tempor dolor."
+        paragraphLabel.text = "This app is designed to help visually impaired people locate objects that they may have lost. There are three main buttons. At the top left, there is an app information button (this page), in the top right is a 'choose items' button in which you can select all the objects that you may want to find. Lastly, there is a large button at the bottom of the page which when pressed will change what object the app looks for from the ones selected in the 'choose items' page. This button also has a simplified 'find all items' setting by default which will read out any items the app finds regarless of whether or not it's in your chosen items."
         
         paragraphLabel.textAlignment = .center
         paragraphLabel.numberOfLines = 0
-        paragraphLabel.font = UIFont.systemFont(ofSize: 22)
+        paragraphLabel.font = UIFont.systemFont(ofSize: 20)
         paragraphLabel.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(paragraphLabel)
         
         NSLayoutConstraint.activate([
             titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            titleLabel.topAnchor.constraint(equalTo: backButton.bottomAnchor, constant: 30),
+            titleLabel.topAnchor.constraint(equalTo: backButton.bottomAnchor, constant: 20),
             paragraphLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            paragraphLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            paragraphLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 15),
             paragraphLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
             paragraphLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
         ])
