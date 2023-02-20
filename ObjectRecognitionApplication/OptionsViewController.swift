dc8232279b1f4844000b5baae99e07b846e36082
Lucas <lucaslemoine49@gmail.com>
2023-02-20 19:43:11 +0000

    grid method was providing more issues so reverted back to original and managed to fix button layout

--- ObjectRecognitionApplication/OptionsViewController.swift
+++ ObjectRecognitionApplication/OptionsViewController.swift
@@ -2,7 +2,8 @@ import UIKit
 
 class OptionsViewController: UIViewController {
 
-    var items = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    var buttons: [UIButton] = []
+    var items = ["a", "b", "c", "d", "e", "f"]
     
     let titleLabel: UILabel = {
         let label = UILabel()
@@ -20,7 +21,7 @@ class OptionsViewController: UIViewController {
         let backButton = UIButton()
         backButton.setTitle("Back", for: .normal)
         backButton.setTitleColor(.white, for: .normal)
-        backButton.backgroundColor = .darkGray
+        backButton.backgroundColor = .black
         backButton.layer.cornerRadius = 10
         backButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
         backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
@@ -45,62 +46,48 @@ class OptionsViewController: UIViewController {
             titleLabel.heightAnchor.constraint(equalToConstant: 50),
         ])
         
+        let buttonTitles = items
         
-        let buttonGrid = GridStack(rows: 3, columns: 2)
-        var buttonIndex = 0
-        for row in 0..<3 {
-            for column in 0..<2 {
-                let button = UIButton(type: .system)
-                let item = items[buttonIndex]
-                button.setTitle(item, for: .normal)
-                button.backgroundColor = .systemBlue
-                button.layer.borderWidth = 1.0
-                button.layer.borderColor = UIColor.black.cgColor
-                button.layer.cornerRadius = 10
-                button.setTitleColor(.white, for: .normal)
-                button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
-                button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-                button.translatesAutoresizingMaskIntoConstraints = false
-                buttonGrid.arrangedSubviews[row].subviews[column].addSubview(button)
-                buttonIndex += 1
-            }
+        for i in 0..<buttonTitles.count {
+            let button = UIButton()
+            button.setTitle(buttonTitles[i], for: .normal)
+            button.backgroundColor = .white
+            button.setTitleColor(.black, for: .normal)
+            button.layer.cornerRadius = 10
+            button.layer.borderColor = UIColor.black.cgColor
+            button.layer.borderWidth = 1
+            button.layer.masksToBounds = true
+            button.tag = i
+            button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+            buttons.append(button)
+            view.addSubview(button)
         }
-
-        buttonGrid.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(buttonGrid)
-        NSLayoutConstraint.activate([
-            buttonGrid.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            buttonGrid.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
-            buttonGrid.widthAnchor.constraint(equalToConstant: 100),
-            buttonGrid.heightAnchor.constraint(equalToConstant: 100),
-        ])
-
         
-
-        let confirmButton = UIButton(type: .system)
-        confirmButton.backgroundColor = .systemBlue
-        confirmButton.layer.borderWidth = 1.0
-        confirmButton.layer.borderColor = UIColor.black.cgColor
-        confirmButton.layer.cornerRadius = 10
-        confirmButton.setTitle("Confirm", for: .normal)
-        confirmButton.setTitleColor(.white, for: .normal)
-        confirmButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
-        confirmButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
-        confirmButton.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(confirmButton)
+    }
         
-        NSLayoutConstraint.activate([
-            confirmButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
-            confirmButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
-            confirmButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-            confirmButton.topAnchor.constraint(equalTo: buttonGrid.bottomAnchor ?? titleLabel.bottomAnchor, constant: 20),
-            confirmButton.heightAnchor.constraint(equalToConstant: 60)
-        ])
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        let buttonWidth = (view.bounds.width - 30) / 2 // subtracting 30 instead of 20 to account for the gap between the buttons
+        let buttonHeight: CGFloat = 60
+        let topMargin = titleLabel.frame.origin.y + titleLabel.frame.size.height + 10.0
+        let numberOfColumns = 2
+        
+        for i in 0..<buttons.count {
+            let row = i / numberOfColumns // integer division to determine row number
+            let col = i % numberOfColumns // modulus to determine column number
+            let button = buttons[i]
+            let x = CGFloat(col) * (buttonWidth + 10) + 10 // adding 10 for gap between buttons
+            let y = topMargin + CGFloat(row) * (buttonHeight + 10)
+            button.frame = CGRect(x: x, y: y, width: buttonWidth, height: buttonHeight)
+        }
     }
 
     
     @IBAction func buttonTapped(_ button: UIButton) {
         print("tapped")
+        button.backgroundColor = button.backgroundColor == .white ? .systemBlue : .white
     }
     
     @objc func backButtonTapped() {
@@ -110,30 +97,4 @@ class OptionsViewController: UIViewController {
         
     }
         
-}
-
-
-class GridStack: UIStackView {
-    init(rows: Int, columns: Int) {
-        super.init(frame: .zero)
-        axis = .vertical
-        alignment = .fill
-        distribution = .fillEqually
-        self.translatesAutoresizingMaskIntoConstraints = false
-        for _ in 0..<rows {
-            let row = UIStackView()
-            row.axis = .horizontal
-            row.alignment = .fill
-            row.distribution = .fillEqually
-            addArrangedSubview(row)
-            for _ in 0..<columns {
-                let view = UIView()
-                row.addArrangedSubview(view)
-            }
-        }
-    }
-    
-    required init(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
 }
