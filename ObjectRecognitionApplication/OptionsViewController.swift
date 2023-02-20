605a35260e67d6aa22a4eb72a358e762a185dd11
Lucas <lucaslemoine49@gmail.com>
2023-02-20 02:07:40 +0000

    added title

--- ObjectRecognitionApplication/OptionsViewController.swift
+++ ObjectRecognitionApplication/OptionsViewController.swift
@@ -1,6 +1,14 @@
 import UIKit
 
 class OptionsViewController: UIViewController {
+
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Select items to locate"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +30,17 @@ class OptionsViewController: UIViewController {
             backButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 20),
             backButton.widthAnchor.constraint(equalToConstant: 150),
             backButton.heightAnchor.constraint(equalToConstant: 80),
+        ])
+        
+        navigationItem.titleView = titleLabel
+        navigationItem.titleView?.contentMode = .center
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
+            titleLabel.topAnchor.constraint(equalTo: backButton.bottomAnchor, constant: 20),
+            titleLabel.widthAnchor.constraint(equalToConstant: view.bounds.width - 40),
+            titleLabel.heightAnchor.constraint(equalToConstant: 50),
         ])
     }
         
