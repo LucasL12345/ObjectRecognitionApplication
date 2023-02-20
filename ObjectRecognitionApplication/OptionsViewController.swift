8a86873b367f758cc630d90d290a4301103cf275
Lucas <lucaslemoine49@gmail.com>
2023-02-20 18:45:57 +0000

    realised there was an issue with layered buttons, remade buttons using grid

--- ObjectRecognitionApplication/OptionsViewController.swift
+++ ObjectRecognitionApplication/OptionsViewController.swift
@@ -2,7 +2,6 @@ import UIKit
 
 class OptionsViewController: UIViewController {
 
-    var buttons: [[UIButton]] = []
     var items = ["a", "b", "c", "d", "e", "f", "g", "h"]
     
     let titleLabel: UILabel = {
@@ -46,34 +45,38 @@ class OptionsViewController: UIViewController {
             titleLabel.heightAnchor.constraint(equalToConstant: 50),
         ])
         
-        let buttonTitles = items
         
-        for i in 0..<buttonTitles.count {
-            let button1 = UIButton()
-            let button2 = UIButton()
-            button1.setTitle(buttonTitles[i], for: .normal)
-            button2.setTitle(buttonTitles[i], for: .normal)
-            button1.backgroundColor = .white
-            button2.backgroundColor = .white
-            button1.setTitleColor(.black, for: .normal)
-            button2.setTitleColor(.black, for: .normal)
-            button1.layer.cornerRadius = 10
-            button2.layer.cornerRadius = 10
-            button1.layer.borderColor = UIColor.black.cgColor
-            button2.layer.borderColor = UIColor.black.cgColor
-            button1.layer.borderWidth = 1
-            button2.layer.borderWidth = 1
-            button1.layer.masksToBounds = true
-            button2.layer.masksToBounds = true
-            button1.tag = i
-            button2.tag = i
-            button1.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-            button2.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-            buttons.append([button1, button2])
-            view.addSubview(button1)
-            view.addSubview(button2)
+        let buttonGrid = GridStack(rows: 3, columns: 2)
+        var buttonIndex = 0
+        for row in 0..<3 {
+            for column in 0..<2 {
+                let button = UIButton(type: .system)
+                let item = items[buttonIndex]
+                button.setTitle(item, for: .normal)
+                button.backgroundColor = .systemBlue
+                button.layer.borderWidth = 1.0
+                button.layer.borderColor = UIColor.black.cgColor
+                button.layer.cornerRadius = 10
+                button.setTitleColor(.white, for: .normal)
+                button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+                button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+                button.translatesAutoresizingMaskIntoConstraints = false
+                buttonGrid.arrangedSubviews[row].subviews[column].addSubview(button)
+                buttonIndex += 1
+            }
         }
+
+        buttonGrid.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(buttonGrid)
+        NSLayoutConstraint.activate([
+            buttonGrid.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            buttonGrid.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            buttonGrid.widthAnchor.constraint(equalToConstant: 100),
+            buttonGrid.heightAnchor.constraint(equalToConstant: 100),
+        ])
+
         
+
         let confirmButton = UIButton(type: .system)
         confirmButton.backgroundColor = .systemBlue
         confirmButton.layer.borderWidth = 1.0
@@ -90,31 +93,10 @@ class OptionsViewController: UIViewController {
             confirmButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
             confirmButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
             confirmButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-            confirmButton.topAnchor.constraint(equalTo: buttons.last?.last?.bottomAnchor ?? titleLabel.bottomAnchor, constant: 20),
+            confirmButton.topAnchor.constraint(equalTo: buttonGrid.bottomAnchor ?? titleLabel.bottomAnchor, constant: 20),
             confirmButton.heightAnchor.constraint(equalToConstant: 60)
         ])
     }
-        
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        
-        let buttonWidth = (view.bounds.width - 20) / 2
-        let buttonHeight: CGFloat = 70
-        let topMargin = titleLabel.frame.origin.y + titleLabel.frame.size.height + 10.0
-        
-        for i in 0..<buttons.count {
-            let buttonPair = buttons[i]
-            let button1 = buttonPair[0]
-            let button2 = buttonPair[1]
-            let isEven = i % 2 == 0
-            
-            button1.frame = CGRect(x: isEven ? 10 : view.bounds.width / 2 + 5, y: topMargin + CGFloat(i / 2) * (buttonHeight + 5), width: buttonWidth, height: buttonHeight)
-            button2.frame = CGRect(x: isEven ? view.bounds.width / 2 + 5 : 10, y: topMargin + CGFloat(i / 2) * (buttonHeight + 5), width: buttonWidth, height: buttonHeight)
-        }
-    }
-
-
 
     
     @IBAction func buttonTapped(_ button: UIButton) {
@@ -128,4 +110,30 @@ class OptionsViewController: UIViewController {
         
     }
         
+}
+
+
+class GridStack: UIStackView {
+    init(rows: Int, columns: Int) {
+        super.init(frame: .zero)
+        axis = .vertical
+        alignment = .fill
+        distribution = .fillEqually
+        self.translatesAutoresizingMaskIntoConstraints = false
+        for _ in 0..<rows {
+            let row = UIStackView()
+            row.axis = .horizontal
+            row.alignment = .fill
+            row.distribution = .fillEqually
+            addArrangedSubview(row)
+            for _ in 0..<columns {
+                let view = UIView()
+                row.addArrangedSubview(view)
+            }
+        }
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
