556c40cc8a8175794fe15a8ba43c571d9022458c
Lucas <lucaslemoine49@gmail.com>
2023-02-20 01:11:35 +0000

    added back button

--- ObjectRecognitionApplication/OptionsViewController.swift
+++ ObjectRecognitionApplication/OptionsViewController.swift
@@ -1,11 +1,36 @@
-
 import UIKit
 
 class OptionsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        
+        self.view.backgroundColor = .white
+        
+        let backButton = UIButton()
+        backButton.setTitle("Back", for: .normal)
+        backButton.setTitleColor(.white, for: .normal)
+        backButton.backgroundColor = .black
+        backButton.layer.cornerRadius = 10
+        backButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(backButton)
+        
+        NSLayoutConstraint.activate([
+            backButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
+            backButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 20),
+            backButton.widthAnchor.constraint(equalToConstant: 150),
+            backButton.heightAnchor.constraint(equalToConstant: 80),
+        ])
     }
+        
     
+    @objc func backButtonTapped() {
+        self.dismiss(animated: true, completion: nil)
+//        let VC = ViewController()
+//        present(VC, animated: true, completion: nil)
+        
+    }
+        
 }
