0235297a67c93bae9d5a1a9fea4f803bc75f81b3
Lucas <lucaslemoine49@gmail.com>
2023-02-20 22:15:56 +0000

    added scrollable functionality to the page

--- ObjectRecognitionApplication/OptionsViewController.swift
+++ ObjectRecognitionApplication/OptionsViewController.swift
@@ -14,9 +14,14 @@ class OptionsViewController: UIViewController {
         return label
     }()
     
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.view.backgroundColor = .white
         
         let backButton = UIButton()
@@ -47,6 +52,14 @@ class OptionsViewController: UIViewController {
             titleLabel.heightAnchor.constraint(equalToConstant: 50),
         ])
         
+        view.addSubview(scrollView)
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
         let buttonTitles = items
         
         // load button colors from user defaults
@@ -70,7 +83,7 @@ class OptionsViewController: UIViewController {
             button.tag = i
             button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
             buttons.append(button)
-            view.addSubview(button)
+            scrollView.addSubview(button)
             
             // set button color from array
             if i < buttonColors.count {
@@ -79,39 +92,14 @@ class OptionsViewController: UIViewController {
                 buttonColors.append(button.backgroundColor ?? .white)
             }
         }
-        
-        let confirmButton = UIButton(type: .system)
-        confirmButton.backgroundColor = .systemBlue
-        confirmButton.layer.borderWidth = 1.0
-        confirmButton.layer.borderColor = UIColor.black.cgColor
-        confirmButton.layer.cornerRadius = 10
-        confirmButton.setTitle("Confirm", for: .normal)
-        confirmButton.setTitleColor(.white, for: .normal)
-        confirmButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
-        confirmButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
-        confirmButton.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(confirmButton)
-
-//        let buttonCount = CGFloat(buttons.count)
-//        let confirmButtonTopAnchor = buttonCount > 0 ? buttons[0].bottomAnchor : titleLabel.bottomAnchor
-//        let confirmButtonHeight = min(120, self.view.safeAreaLayoutGuide.layoutFrame.height * 0.2, (self.view.safeAreaLayoutGuide.layoutFrame.maxY - confirmButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10).constant - 20) / CGFloat(buttonCount))
-
-        NSLayoutConstraint.activate([
-            confirmButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
-            confirmButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
-            confirmButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-            confirmButton.heightAnchor.constraint(equalToConstant: 120)
-        ])
     }
-    
-        
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         
         let buttonWidth = (view.bounds.width - 30) / 2 // subtracting 30 instead of 20 to account for the gap between the buttons
         let buttonHeight: CGFloat = 60
-        let topMargin = titleLabel.frame.origin.y + titleLabel.frame.size.height + 10.0
+        let topMargin: CGFloat = 10.0
         let numberOfColumns = 2
         
         for i in 0..<buttons.count {
@@ -122,7 +110,11 @@ class OptionsViewController: UIViewController {
             let y = topMargin + CGFloat(row) * (buttonHeight + 10)
             button.frame = CGRect(x: x, y: y, width: buttonWidth, height: buttonHeight)
         }
+        
+        scrollView.contentSize = CGSize(width: scrollView.bounds.width, height: topMargin + CGFloat((buttons.count + 1) / 2) * (buttonHeight + 10))
     }
+
+
 
     
     @IBAction func buttonTapped(_ button: UIButton) {
