039948387b48c7b5135ad4245123571bf06cf5e2
Lucas <lucaslemoine49@gmail.com>
2023-02-20 17:42:01 +0000

    changed button layout to match wireframes

--- ObjectRecognitionApplication/OptionsViewController.swift
+++ ObjectRecognitionApplication/OptionsViewController.swift
@@ -2,8 +2,8 @@ import UIKit
 
 class OptionsViewController: UIViewController {
 
-    var buttons: [UIButton] = []
-    var items = ["x", "x", "x", "x", "x", "x"]
+    var buttons: [[UIButton]] = []
+    var items = ["a", "b", "c", "d", "e", "f", "g", "h"]
     
     let titleLabel: UILabel = {
         let label = UILabel()
@@ -49,35 +49,54 @@ class OptionsViewController: UIViewController {
         let buttonTitles = items
         
         for i in 0..<buttonTitles.count {
-            let button = UIButton()
-            button.setTitle(buttonTitles[i], for: .normal)
-            button.backgroundColor = .white
-            button.setTitleColor(.black, for: .normal)
-            button.layer.cornerRadius = 10
-            button.layer.borderColor = UIColor.black.cgColor
-            button.layer.borderWidth = 1
-            button.layer.masksToBounds = true
-            button.tag = i
-            button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-            buttons.append(button)
-            view.addSubview(button)
+            let button1 = UIButton()
+            let button2 = UIButton()
+            button1.setTitle(buttonTitles[i], for: .normal)
+            button2.setTitle(buttonTitles[i], for: .normal)
+            button1.backgroundColor = .white
+            button2.backgroundColor = .white
+            button1.setTitleColor(.black, for: .normal)
+            button2.setTitleColor(.black, for: .normal)
+            button1.layer.cornerRadius = 10
+            button2.layer.cornerRadius = 10
+            button1.layer.borderColor = UIColor.black.cgColor
+            button2.layer.borderColor = UIColor.black.cgColor
+            button1.layer.borderWidth = 1
+            button2.layer.borderWidth = 1
+            button1.layer.masksToBounds = true
+            button2.layer.masksToBounds = true
+            button1.tag = i
+            button2.tag = i
+            button1.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+            button2.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+            buttons.append([button1, button2])
+            view.addSubview(button1)
+            view.addSubview(button2)
         }
         
     }
         
     
     override func viewWillLayoutSubviews() {
-            super.viewWillLayoutSubviews()
+        super.viewWillLayoutSubviews()
+        
+        let buttonWidth = (view.bounds.width - 20) / 2
+        let buttonHeight: CGFloat = 70
+        let topMargin = titleLabel.frame.origin.y + titleLabel.frame.size.height + 10.0
+        
+        for i in 0..<buttons.count {
+            let buttonPair = buttons[i]
+            let button1 = buttonPair[0]
+            let button2 = buttonPair[1]
+            let isEven = i % 2 == 0
             
-            let buttonWidth = view.bounds.width - 20
-            let buttonHeight: CGFloat = 60
-            let topMargin = titleLabel.frame.origin.y + titleLabel.frame.size.height + 10.0
-            
-            for i in 0..<buttons.count {
-                let button = buttons[i]
-                button.frame = CGRect(x: 10, y: topMargin + CGFloat(i) * (buttonHeight + 10), width: buttonWidth, height: buttonHeight)
-            }
+            button1.frame = CGRect(x: isEven ? 10 : view.bounds.width / 2 + 5, y: topMargin + CGFloat(i / 2) * (buttonHeight + 5), width: buttonWidth, height: buttonHeight)
+            button2.frame = CGRect(x: isEven ? view.bounds.width / 2 + 5 : 10, y: topMargin + CGFloat(i / 2) * (buttonHeight + 5), width: buttonWidth, height: buttonHeight)
         }
+    }
+
+
+
     
     @IBAction func buttonTapped(_ button: UIButton) {
         print("tapped")
